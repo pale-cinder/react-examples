675fb5c871b5631f0a888427c8e2d64bfcf0c8b1
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-11-21 19:37:25 -0800

    testing return function

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hello, I am a React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hello, I am a React App</h1>
+    //   </div>
+    // );
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I am a React'));
   }
 }
 
