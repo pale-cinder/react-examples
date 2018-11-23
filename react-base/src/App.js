2ca0423890a1492c510f7c5fa0319474b6c26bd0
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-11-22 17:31:47 -0800

    props

--- react-base/src/App.js
+++ react-base/src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
       <div className="App">
         <h1>Hello, I am a React App</h1>
         <p>This is something workable</p>
+
+        <h1>Let's introduce ourselves!</h1>
         <Person />
         <Person />
         <Person />
         <Person />
         <Person />
+        <h1>Let's talk!</h1>
+        <Person name="Cinder" age="28"/>
+        <Person name="Weak" age="29" >My hobbies: Coding</Person>
+        <Person name="Albus" age="6"/>
+
       </div>
     );
 
--- react-base/src/Person/Person.js
+++ react-base/src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a Person :( and I am {Math.floor(Math.random() * 28)} years old</p>
+const person = (props) => {
+    //{Math.floor(Math.random() * 28)} -- this is random age calculation
+    //return <p>I'm {props.name} a Person :( and I am {Math.floor(Math.random() * 28)} years old</p>
+    return (
+        <div>
+             <p>I'm {props.name} a Person :( and I am {props.age} years old</p>
+             <p>{props.children}</p>
+        </div>
+   
+    )
 };
 
 export default person;
