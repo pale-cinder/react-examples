6a1429c8b7107795b621f9c33aa03721f635f852
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-11-24 15:26:25 -0800

    styling

--- react-base/src/Person/Person.js
+++ react-base/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     //{Math.floor(Math.random() * 28)} -- this is random age calculation
     //return <p>I'm {props.name} a Person :( and I am {Math.floor(Math.random() * 28)} years old</p>
     return (
-        <div>
+        <div className="Person">
              <p onClick={props.click}>I'm {props.name} a Person :( and I am {props.age} years old</p>
              <p>{props.children}</p>
              <input type="text" onChange={props.changed} value={props.name} />
