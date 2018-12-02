3b6abd4fda8552b9e54dac5547ae5e75d932e58e
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-01 16:37:03 -0800

    fix

--- react-base/src/Person/Person.js
+++ react-base/src/Person/Person.js
@@ -14,7 +14,8 @@ const person = (props) => {
     
     return (
         //<div className="Person" style={style}>
-        <div className="Person" 
+        <div className={classes.Person}>
+        {/* </div><div className="Person"  */}
              <p onClick={props.click}>I'm {props.name} a Person :( and I am {props.age} years old</p>
              <p>{props.children}</p>
              <input type="text" onChange={props.changed} value={props.name} />
