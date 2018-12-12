865cd04dcbfe06a39d2549d4ce66d066240a0ca8
Yuliia Zinchenko <pale.allusion@gmail.com>
2018-12-11 18:19:58 -0800

    header and mq done

--- compare-react/src/components/Footer/Footer.js
+++ compare-react/src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // stateless component
+
 const Footer = () => {
 
     return (
--- compare-react/src/components/Header/Header.js
+++ compare-react/src/components/Header/Header.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-       my header
+      <div className="nav has-shadow">
+  <div className="container">
+    <div className="nav-left">
+      <a routerLink="/" className="nav-item">MyCompany</a>
+    </div>
+    <span className="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+
+    <div className="nav-right nav-menu">
+
+      {/* <a routerLink="/" className="nav-item r-item">Home</a>
+      <a className="nav-item r-item">About</a>
+      <a className="nav-item r-item">Features</a>
+      <a routerLink="/faq" className="nav-item r-item">FAQ</a> */}
+
+        <div className="nav-item">
+          <p className="control">
+            <a className="button is-primary is-outlined">
+              <span className="icon">
+                <i className="fa fa-download"></i>
+              </span>
+              <span>Join Now</span>
+            </a>
+          </p>
+        </div>
       </div>
+  </div>
+</div>
+
     );
   }
 }
