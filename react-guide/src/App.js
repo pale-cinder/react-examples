9a794eee8e0ca5a4411aaacf133ee161aa70d287
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-09-09 14:15:02 -0700

    create element

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hello world!</h1>
+  //   </div>
+  // );
+  return React.createElement('div', null, 'h1', 'Hello React');
 }
 
 export default App;
