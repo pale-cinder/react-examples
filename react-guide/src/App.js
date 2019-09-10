09b07a5e1b927c422f1870dc2c612176ae91c386
Yuliia Zinchenko <pale.allusion@gmail.com>
2019-09-09 21:12:30 -0700

    useState React Hooks

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,45 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 //function App() {
-  class App extends Component {
-  state = {
-    persons: [
-      { name: 'Harry', age: 18 },
-      { name: 'Hermione', age: 17 },
-      { name: 'Albus', age: 1 }
-    ],
-    otherState: 'something else'
-  }
-  
-  switchHandler = () => {
-    //this.state.persons[0].name = 'Potter';
-    //console.log('clicked');
-    this.setState( { //it will override the state but will not touch something that is not changed
+  const app = props => {
+    const [ personsState, setPersonsState ] = useState({ //useState returns always two elements and only two elements
       persons: [
-      { name: 'Harry Potter', age: 18 },
-      { name: 'Hermione', age: 17 },
-      { name: 'Albus', age: 1 }
-      ]
-    } )
-  }
+        { name: 'Harry', age: 18 },
+        { name: 'Hermione', age: 17 },
+        { name: 'Albus', age: 1 }
+      ],
+      otherState: 'something else'
+    });
 
-  render(){
+    const [otherState, setOtherState] = useState('something else');
+
+    const switchHandler = () => {
+      //this.state.persons[0].name = 'Potter';
+      //console.log('clicked');
+      setPersonsState({ //it will override the state but will not touch something that is not changed
+        persons: [
+        { name: 'Harry Potter', age: 18 },
+        { name: 'Hermione', age: 17 },
+        { name: 'Albus', age: 1 }
+        ],
+      })
+    }
+    
+  
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <button onClick={this.switchHandler}>Switch Name</button>
-      <Person name={this.state.persons[1].name} age="18" />
-      <Person name={this.state.persons[0].name} age="17">My hobbies: Quidditch</Person>
-      <Person name={this.state.persons[2].name} age="1" />
+      <button onClick={switchHandler}>Switch Name</button>
+      <Person 
+        name={setPersonsState.persons[1].name} 
+        age="18" />
+
+      <Person 
+        name={setPersonsState.persons[0].name} 
+        age="17">
+        My hobbies: Quidditch
+      </Person>
+
+      <Person 
+        name={setPersonsState.persons[2].name} 
+        age="1" />
+
     </div>
   );
-  }
-  // nested React element
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello React works'));
-
 }
 
-export default App;
+export default app;
